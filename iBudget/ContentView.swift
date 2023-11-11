06ba514b7445dc29d8a703757f74da4f26a6455d
himpensa <33318648+himpensa@users.noreply.github.com>
2023-11-11 19:55:27 +0100

    Initial Commit

---
+++ iBudget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iBudget
+//
+//  Created by Antoine Himpens on 11/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iBudget/iBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBudgetApp.swift
+//  iBudget
+//
+//  Created by Antoine Himpens on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct iBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
