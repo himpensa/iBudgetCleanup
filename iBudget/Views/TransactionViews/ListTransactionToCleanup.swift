777549298ba10d187ad4d581bfff08f150f08b9f
himpensa <33318648+himpensa@users.noreply.github.com>
2024-03-12 21:52:14 +0100

    CategoriesCleanup

---
+++ iBudget/Views/TransactionViews/ListTransactionToCleanup.swift
@@ -0,0 +1,18 @@
+//
+//  ListTransactionToCleanup.swift
+//  iBudget
+//
+//  Created by Antoine Himpens on 12/03/2024.
+//
+
+import SwiftUI
+
+struct ListTransactionToCleanup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListTransactionToCleanup()
+}
